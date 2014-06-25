34bb67657399f0daa0dd21b7a00bdac26d0b6be4
pepillus <matiasd.perez@gmail.com>
2014-06-25 15:21:02 -0300

    9

--- GUISwing/src/com/scoreboard/gui/swing/VentanaPrincipal.java
+++ GUISwing/src/com/scoreboard/gui/swing/VentanaPrincipal.java
@@ -9,6 +9,7 @@ import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
@@ -23,9 +24,11 @@ import com.scoreboard.managers.MatchManager;
 
 public class VentanaPrincipal extends JFrame 
 {	
+	MatchManager mma;
+	private int sets;
 	private JPanel plJugadoresLbl,
-					plPuntosLbl,
 					plGamesLbl,
+					plMatchLbl,
 					plSet1Lbl,
 					plSet2Lbl,
 					plSet3Lbl,
@@ -34,171 +37,238 @@ public class VentanaPrincipal extends JFrame
 					plJugadorUnoLbl,
 					plJugadorDosLbl,
 					plJugadorUno,
-					plPuntosJugadorUno,
-					plGameJugadorUno,					
+					plGamesJugadorUno,
+					plMatchJugadorUno,					
 					plSet1JugadorUno,
 					plSet2JugadorUno,
 					plSet3JugadorUno,
 					plSet4JugadorUno,
 					plSet5JugadorUno,			
 					plJugadorDos,
-					plPuntosJugadorDos,
-					plGameJugadorDos,					
+					plGamesJugadorDos,
+					plMatchJugadorDos,					
 					plSet1JugadorDos,
 					plSet2JugadorDos,
 					plSet3JugadorDos,
 					plSet4JugadorDos,
 					plSet5JugadorDos;
 	
-	public VentanaPrincipal()//Le pasarias cantidad de sets para settear el layout
+	private JLabel lblJugadorUno,
+					gamesJugadorUno,
+					matchJugadorUno,
+					set1JugadorUno,
+					set2JugadorUno,
+					set3JugadorUno,
+					set4JugadorUno,
+					set5JugadorUno,
+					lblJugadorDos,
+					gamesJugadorDos,
+					matchJugadorDos,
+					set1JugadorDos,
+					set2JugadorDos,
+					set3JugadorDos,
+					set4JugadorDos,
+					set5JugadorDos;	
+	
+	
+	public VentanaPrincipal(int sets)//Le pasarias cantidad de sets para settear el layout
 	{
+		this.sets = sets;	
+		mma = new MatchManager();
+		mma.newMatch("", sets);
 		setSize(783, 223);
 		setTitle("Tenis");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
-		setLayout(new GridLayout(3,6));	
+		if(sets == 3)
+			setLayout(new GridLayout(3,6));
+		else
+			setLayout(new GridLayout(3,8));
 		
 		//Primera Fila
 		plJugadoresLbl = new JPanel();
-		plPuntosLbl = new JPanel();
 		plGamesLbl = new JPanel();
+		plMatchLbl = new JPanel();
 		plSet1Lbl = new JPanel();
 		plSet2Lbl = new JPanel();
 		plSet3Lbl = new JPanel();
-		//plSet4Lbl = new JPanel();
-		//plSet5Lbl = new JPanel();
+		if(sets > 3)
+		{
+			plSet4Lbl = new JPanel();
+			plSet5Lbl = new JPanel();
+		}
 		
 		plJugadoresLbl.setBorder(BorderFactory.createLineBorder(Color.black));
-		plPuntosLbl.setBorder(BorderFactory.createLineBorder(Color.black));
 		plGamesLbl.setBorder(BorderFactory.createLineBorder(Color.black));
+		plMatchLbl.setBorder(BorderFactory.createLineBorder(Color.black));
 		plSet1Lbl.setBorder(BorderFactory.createLineBorder(Color.black));
 		plSet2Lbl.setBorder(BorderFactory.createLineBorder(Color.black));
 		plSet3Lbl.setBorder(BorderFactory.createLineBorder(Color.black));
-		//plSet4Lbl.setBorder(BorderFactory.createLineBorder(Color.black));
-		//plSet5Lbl.setBorder(BorderFactory.createLineBorder(Color.black));
+		if(sets > 3)
+		{
+			plSet4Lbl.setBorder(BorderFactory.createLineBorder(Color.black));
+			plSet5Lbl.setBorder(BorderFactory.createLineBorder(Color.black));
+		}
 		
 		plJugadoresLbl.add(new JLabel("Jugadores"));	
-		plPuntosLbl.add(new JLabel("Puntos"));
-		plGamesLbl.add(new JLabel("Games"));
+		plGamesLbl.add(new JLabel("Game"));
+		plMatchLbl.add(new JLabel("Match"));
 		plSet1Lbl.add(new JLabel("Set 1"));
 		plSet2Lbl.add(new JLabel("Set 2"));
 		plSet3Lbl.add(new JLabel("Set 3"));
-		//plSet4Lbl.add(new JLabel("Set 4"));
-		//plSet5Lbl.add(new JLabel("Set 5"));
+		if(sets > 3)
+		{
+			plSet4Lbl.add(new JLabel("Set 4"));
+			plSet5Lbl.add(new JLabel("Set 5"));
+		}
 	
 		add(plJugadoresLbl);		
-		add(plPuntosLbl);
 		add(plGamesLbl);
+		add(plMatchLbl);
 		add(plSet1Lbl);
 		add(plSet2Lbl);
 		add(plSet3Lbl);
-		//add(plSet4Lbl);
-		//add(plSet5Lbl);
+		if(sets > 3)
+		{
+			add(plSet4Lbl);
+			add(plSet5Lbl);
+		}
 	
 		//Segunda Fila	
 		JButton btnJugadorUno = new JButton("Punto");
 		plJugadorUno = new JPanel();
-		plPuntosJugadorUno = new JPanel();
-		plGameJugadorUno = new JPanel();
+		plGamesJugadorUno = new JPanel();
+		plMatchJugadorUno = new JPanel();
 		plSet1JugadorUno = new JPanel();
 		plSet2JugadorUno = new JPanel();
 		plSet3JugadorUno = new JPanel();
-		//plSet4JugadorUno = new JPanel();
-		//plSet5JugadorUno = new JPanel();
+		if(sets > 3)
+		{
+			plSet4JugadorUno = new JPanel();
+			plSet5JugadorUno = new JPanel();
+		}
 		
 		plJugadorUno.setBorder(BorderFactory.createLineBorder(Color.GRAY));
-		plPuntosJugadorUno.setBorder(BorderFactory.createLineBorder(Color.black));
-		plGameJugadorUno.setBorder(BorderFactory.createLineBorder(Color.black));
+		plGamesJugadorUno.setBorder(BorderFactory.createLineBorder(Color.black));
+		plMatchJugadorUno.setBorder(BorderFactory.createLineBorder(Color.black));
 		plSet1JugadorUno.setBorder(BorderFactory.createLineBorder(Color.black));
 		plSet2JugadorUno.setBorder(BorderFactory.createLineBorder(Color.black));
 		plSet3JugadorUno.setBorder(BorderFactory.createLineBorder(Color.black));
-		//plSet4JugadorUno.setBorder(BorderFactory.createLineBorder(Color.black));
-		//plSet5JugadorUno.setBorder(BorderFactory.createLineBorder(Color.black));
+		if(sets > 3)
+		{
+			plSet4JugadorUno.setBorder(BorderFactory.createLineBorder(Color.black));
+			plSet5JugadorUno.setBorder(BorderFactory.createLineBorder(Color.black));
+		}
 		
-		JLabel lblJugadorUno = new JLabel("Pepe");
-		JLabel puntosJugadorUno = new JLabel("0");
-		JLabel gamesJugadorUno = new JLabel("0");
-		JLabel set1JugadorUno = new JLabel("0");
-		JLabel set2JugadorUno = new JLabel("0");
-		JLabel set3JugadorUno = new JLabel("0");
-		//JLabel set4JugadorUno = new JLabel("0");
-		//JLabel set5JugadorUno = new JLabel("0");		
+		lblJugadorUno = new JLabel("Pepe");
+		gamesJugadorUno = new JLabel("0");
+		matchJugadorUno = new JLabel("0");
+		set1JugadorUno = new JLabel("0");
+		set2JugadorUno = new JLabel("0");
+		set3JugadorUno = new JLabel("0");
+		if(sets > 3)
+		{
+			set4JugadorUno = new JLabel("0");
+			set5JugadorUno = new JLabel("0");
+		}
 		
-		plPuntosJugadorUno.add(puntosJugadorUno);	
+		plGamesJugadorUno.add(gamesJugadorUno);	
 		plJugadorUno.add(lblJugadorUno);
 		plJugadorUno.add(btnJugadorUno);		
-		plGameJugadorUno.add(gamesJugadorUno);
+		plMatchJugadorUno.add(matchJugadorUno);
 		plSet1JugadorUno.add(set1JugadorUno);
 		plSet2JugadorUno.add(set2JugadorUno);
 		plSet3JugadorUno.add(set3JugadorUno);
-		//plSet4JugadorUno.add(set4JugadorUno);
-		//plSet5JugadorUno.add(set5JugadorUno);		
+		if(sets > 3)
+		{
+			plSet4JugadorUno.add(set4JugadorUno);
+			plSet5JugadorUno.add(set5JugadorUno);
+		}
 		
 		add(plJugadorUno);
-		add(plPuntosJugadorUno);
-		add(plGameJugadorUno);
+		add(plGamesJugadorUno);
+		add(plMatchJugadorUno);
 		add(plSet1JugadorUno);
 		add(plSet2JugadorUno);
 		add(plSet3JugadorUno);
-		//add(plSet4JugadorUno);
-		//add(plSet5JugadorUno);
+		if(sets > 3)
+		{
+			add(plSet4JugadorUno);
+			add(plSet5JugadorUno);
+		}
 		
 		//Tercera Fila
 		JButton btnJugadorDos = new JButton("Punto");
 		plJugadorDos = new JPanel();
-		plPuntosJugadorDos = new JPanel();
-		plGameJugadorDos = new JPanel();
+		plGamesJugadorDos = new JPanel();
+		plMatchJugadorDos = new JPanel();
 		plSet1JugadorDos = new JPanel();
 		plSet2JugadorDos = new JPanel();
 		plSet3JugadorDos = new JPanel();
-		//plSet4JugadorDos = new JPanel();
-		//plSet5JugadorDos = new JPanel();
+		if(sets > 3)
+		{
+			plSet4JugadorDos = new JPanel();
+			plSet5JugadorDos = new JPanel();
+		}
 		
 		plJugadorDos.setBorder(BorderFactory.createLineBorder(Color.GRAY));
-		plPuntosJugadorDos.setBorder(BorderFactory.createLineBorder(Color.black));
-		plGameJugadorDos.setBorder(BorderFactory.createLineBorder(Color.black));
+		plGamesJugadorDos.setBorder(BorderFactory.createLineBorder(Color.black));
+		plMatchJugadorDos.setBorder(BorderFactory.createLineBorder(Color.black));
 		plSet1JugadorDos.setBorder(BorderFactory.createLineBorder(Color.black));
 		plSet2JugadorDos.setBorder(BorderFactory.createLineBorder(Color.black));
 		plSet3JugadorDos.setBorder(BorderFactory.createLineBorder(Color.black));
-		//plSet4JugadorDos.setBorder(BorderFactory.createLineBorder(Color.black));
-		//plSet5JugadorDos.setBorder(BorderFactory.createLineBorder(Color.black));
+		if(sets > 3)
+		{
+			plSet4JugadorDos.setBorder(BorderFactory.createLineBorder(Color.black));
+			plSet5JugadorDos.setBorder(BorderFactory.createLineBorder(Color.black));
+		}
 		
-		JLabel lblJugadorDos = new JLabel("Coco");
-		JLabel puntosJugadorDos = new JLabel("0");
-		JLabel gamesJugadorDos = new JLabel("0");
-		JLabel set1JugadorDos = new JLabel("0");
-		JLabel set2JugadorDos = new JLabel("0");
-		JLabel set3JugadorDos = new JLabel("0");
-		//JLabel set4JugadorDos = new JLabel("0");
-		//JLabel set5JugadorDos = new JLabel("0");		
+		lblJugadorDos = new JLabel("Coco");
+		gamesJugadorDos = new JLabel("0");
+		matchJugadorDos = new JLabel("0");
+		set1JugadorDos = new JLabel("0");
+		set2JugadorDos = new JLabel("0");
+		set3JugadorDos = new JLabel("0");
+		if(sets > 3)
+		{
+			set4JugadorDos = new JLabel("0");
+			set5JugadorDos = new JLabel("0");
+		}
 		
-		plPuntosJugadorDos.add(puntosJugadorDos);
+		plGamesJugadorDos.add(gamesJugadorDos);
 		plJugadorDos.add(lblJugadorDos);
 		plJugadorDos.add(btnJugadorDos);
-		plGameJugadorDos.add(gamesJugadorDos);
+		plMatchJugadorDos.add(matchJugadorDos);
 		plSet1JugadorDos.add(set1JugadorDos);
 		plSet2JugadorDos.add(set2JugadorDos);
 		plSet3JugadorDos.add(set3JugadorDos);
-		//plSet4JugadorUno.add(set4JugadorDos);
-		//plSet5JugadorUno.add(set5JugadorDos);		
+		if(sets > 3)
+		{
+			plSet4JugadorDos.add(set4JugadorDos);
+			plSet5JugadorDos.add(set5JugadorDos);
+		}
 		
 		add(plJugadorDos);
-		add(plPuntosJugadorDos);
-		add(plGameJugadorDos);
+		add(plGamesJugadorDos);
+		add(plMatchJugadorDos);
 		add(plSet1JugadorDos);
 		add(plSet2JugadorDos);
 		add(plSet3JugadorDos);
-		//add(plSet4JugadorDos);
-		//add(plSet5JugadorDos);
+		if(sets > 3)
+		{
+			add(plSet4JugadorDos);
+			add(plSet5JugadorDos);
+		}
 		
 				
 		btnJugadorUno.addActionListener(new ActionListener()
 		{			
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-//				MatchManager mm = new MatchManager();
-//				mm.addPoint(true);
-//				actualizarPartido();
+				mma.addPoint(true);
+				actualizarPartido(mma.getCurrentScore());
+				if(mma.hasWinner())
+					System.out.println("GANO "+mma.getWinner().getNombre());
 			}
 		});
 		
@@ -206,17 +276,47 @@ public class VentanaPrincipal extends JFrame
 		{			
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-//				MatchManager mm = new MatchManager();
-//				mm.newMatch("C:\\", 3);
-//				mm.addPoint(false);
-//				actualizarPartido();
+				mma.addPoint(false);
+				actualizarPartido(mma.getCurrentScore());
+				if(mma.hasWinner())
+					System.out.println("GANO "+mma.getWinner().getNombre());
+				 
 			}
 		});	
 	}
 	
-	private void actualizarPartido()
-	{		
+	private void actualizarPartido(ArrayList<String[]> puntajes)
+	{	
+		if(sets > 3)
+		{
+			gamesJugadorUno.setText(puntajes.get(6)[0].toString());
+			gamesJugadorDos.setText(puntajes.get(6)[1].toString());		
+			matchJugadorUno.setText(puntajes.get(0)[0].toString());
+			matchJugadorDos.setText(puntajes.get(0)[1].toString());		
+			set1JugadorUno.setText(puntajes.get(1)[0].toString());
+			set1JugadorDos.setText(puntajes.get(1)[1].toString());		
+			set2JugadorUno.setText(puntajes.get(2)[0].toString());
+			set2JugadorDos.setText(puntajes.get(2)[1].toString());		
+			set3JugadorUno.setText(puntajes.get(3)[0].toString());
+			set3JugadorDos.setText(puntajes.get(3)[1].toString());			
+			set4JugadorUno.setText(puntajes.get(4)[0].toString());
+			set4JugadorDos.setText(puntajes.get(4)[1].toString());		
+			set5JugadorUno.setText(puntajes.get(5)[0].toString());
+			set5JugadorDos.setText(puntajes.get(5)[1].toString());
+		}
+		else
+		{		
+			gamesJugadorUno.setText(puntajes.get(4)[0].toString());
+			gamesJugadorDos.setText(puntajes.get(4)[1].toString());		
+			matchJugadorUno.setText(puntajes.get(0)[0].toString());
+			matchJugadorDos.setText(puntajes.get(0)[1].toString());		
+			set1JugadorUno.setText(puntajes.get(1)[0].toString());
+			set1JugadorDos.setText(puntajes.get(1)[1].toString());		
+			set2JugadorUno.setText(puntajes.get(2)[0].toString());
+			set2JugadorDos.setText(puntajes.get(2)[1].toString());		
+			set3JugadorUno.setText(puntajes.get(3)[0].toString());
+			set3JugadorDos.setText(puntajes.get(3)[1].toString());
+		}
 		
 	}
-
 }
